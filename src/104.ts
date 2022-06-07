5df29ce64fe4aae3e7d9a92bb7c0c55b12786be7
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2022-06-08 01:42:17 +0800

    update: 104

--- src/104.ts
+++ src/104.ts
@@ -1,19 +1,21 @@
-const maxDepth = (root: TreeNode | null): number => {
-    if (root === null) {
-        return 0
-    }
-    let max: number = 1
-    const helper = (node: TreeNode, depth: number = 1) => {
-        max = Math.max(max, depth)
-        if (node.left) {
-            helper(node.left, depth + 1)
-        }
-        if (node.right) {
-            helper(node.right, depth + 1)
-        }
-    }
-    helper(root)
-    return max
-}
+// const maxDepth = (root: TreeNode | null): number => {
+//     if (root === null) {
+//         return 0
+//     }
+//     let max: number = 1
+//     const helper = (node: TreeNode, depth: number = 1) => {
+//         max = Math.max(max, depth)
+//         if (node.left) {
+//             helper(node.left, depth + 1)
+//         }
+//         if (node.right) {
+//             helper(node.right, depth + 1)
+//         }
+//     }
+//     helper(root)
+//     return max
+// }
+
+const maxDepth = (root: TreeNode | null): number => root === null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
 
 export default maxDepth
