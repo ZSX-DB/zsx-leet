52a42b88b126447eb45835e589f025fec2e2e58f
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2022-08-13 13:38:25 +0800

    add: 1688

---
+++ src/1688.ts
@@ -0,0 +1,12 @@
+const numberOfMatches = (n: number): number => n - 1
+
+// const numberOfMatches = (n: number): number => {
+//     let count = 0
+//     while (n !== 1) {
+//         count += Math.floor(n / 2)
+//         n = Math.ceil(n / 2)
+//     }
+//     return count
+// }
+
+export default numberOfMatches
---
+++ src/tests/1688.spec.ts
@@ -0,0 +1,6 @@
+import numberOfMatches from '../1688';
+
+test("1688", () => {
+    expect(numberOfMatches(7)).toBe(6)
+    expect(numberOfMatches(14)).toBe(13)
+})
