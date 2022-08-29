b997f4f64940d29542f85f9bb39e73becac39e07
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2022-08-29 23:46:35 +0800

    update: 933

--- src/tests/933.spec.ts
+++ src/tests/933.spec.ts
@@ -6,6 +6,7 @@ test("933", () => {
     expect(rc1.ping(100)).toBe(2)
     expect(rc1.ping(3001)).toBe(3)
     expect(rc1.ping(3002)).toBe(3)
+    
     const rc2 = new RecentCounter()
     expect(rc2.ping(642)).toBe(1)
     expect(rc2.ping(1849)).toBe(2)
