0289fb3bd7f669380005adb28536464326ca5040
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2022-08-31 00:49:38 +0800

    update: convert function

--- src/tests/convert.spec.ts
+++ src/tests/convert.spec.ts
@@ -1,4 +1,4 @@
-import {toBinaryTree, toLinkedList} from "../utils/convert";
+import { toBinaryTree, toLinkedList } from "../utils/convert";
 
 test("toBinaryTree", () => {
     expect(toBinaryTree([1, 2])).toEqual({
@@ -88,10 +88,47 @@ test("toBinaryTree", () => {
             },
         }
     })
+    expect(toBinaryTree([1, null, 2, null, 3, null, 4, null, 5, null, 6, null, 7, null, 8, null, 9])).toEqual({
+        val: 1,
+        left: null,
+        right: {
+            val: 2,
+            left: null,
+            right: {
+                val: 3,
+                left: null,
+                right: {
+                    val: 4,
+                    left: null,
+                    right: {
+                        val: 5,
+                        left: null,
+                        right: {
+                            val: 6,
+                            left: null,
+                            right: {
+                                val: 7,
+                                left: null,
+                                right: {
+                                    val: 8,
+                                    left: null,
+                                    right: {
+                                        val: 9,
+                                        left: null,
+                                        right: null
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
 })
 
 test("toLinkedList", () => {
     expect(toLinkedList([])).toEqual(null)
-    expect(toLinkedList([3, 2, 6])).toEqual({val: 3, next: {val: 2, next: {val: 6, next: null}}})
-    expect(toLinkedList([3, 1, 2, 4])).toEqual({val: 3, next: {val: 1, next: {val: 2, next: {val: 4, next: null}}}})
+    expect(toLinkedList([3, 2, 6])).toEqual({ val: 3, next: { val: 2, next: { val: 6, next: null } } })
+    expect(toLinkedList([3, 1, 2, 4])).toEqual({ val: 3, next: { val: 1, next: { val: 2, next: { val: 4, next: null } } } })
 })
--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -2,64 +2,35 @@ type Tree = number | null
 
 type Trees = Tree[]
 
-const fillTrees = (trees: Trees): Trees => {
-    if (trees.includes(null)) {
-        const maxPossibleDepth: number = trees.filter(tree => tree !== null).length
-        const wholeTrees: Trees = [...trees]
-        for (let i = 0; i < maxPossibleDepth; i++) {
-            if (wholeTrees[i] === null) {
-                wholeTrees.splice(2 * i + 1, 0, null, null)
-            }
-        }
-        return wholeTrees
-    } else {
-        let len: number = trees.length
-        let num: number = 1
-        while (len > num) {
-            len -= num
-            num *= 2
-        }
-        return trees.concat(Array(num * 3 - len).fill(null))
-    }
-}
-
-const hierarchyTrees = (trees: Trees): Trees[] => {
-    const hierarchies: Trees[] = []
-    let sliceLen: number = 1
-    let start: number = 0
-    while (sliceLen < trees.length) {
-        const hierarchy = []
-        for (let i = start; i < start + sliceLen; i++) {
-            hierarchy.push(trees[i] ?? null)
-        }
-        if (hierarchy.every(item => item === null)) {
-            break
-        }
-        hierarchies.push(hierarchy)
-        start += sliceLen
-        sliceLen *= 2
-    }
-    return hierarchies.reverse()
-}
-
 const toBinaryTree = (trees: Trees): TreeNode | null => {
     if (trees.length === 0) {
         return null
     }
-    // 填充缺失的 null 节点
-    const wholeTrees: Trees = fillTrees(trees)
-    // 生成自底向上树
-    const hierarchies: Trees[] = hierarchyTrees(wholeTrees)
-    // 存放节点
-    const container: Array<TreeNode | null>[] = []
-    // 构建树
-    for (let i = 0; i < hierarchies.length; i++) {
-        const hierarchy: Trees = hierarchies[i]
+    // 根据二维数组反向构建二叉树
+    const items: Trees[] = [[trees.shift() as Tree]]
+    while (trees.length) {
+        const firstItem = items[0]
+        if (firstItem.every(item => item === null)) {
+            break
+        }
+        const newItem: Trees = []
+        firstItem.forEach(item => {
+            if (item === null) {
+                newItem.push(null, null)
+            } else {
+                newItem.push(trees.shift() ?? null, trees.shift() ?? null)
+            }
+        })
+        items.unshift(newItem)
+    }
+    const container: (TreeNode | null)[][] = []
+    for (let i = 0; i < items.length; i++) {
+        const item: Trees = items[i]
         if (i === 0) {
-            container.push(hierarchy.map(item => item === null ? null : {val: item, left: null, right: null}))
+            container.push(item.map(it => it === null ? null : { val: it, left: null, right: null }))
         } else {
-            container.push(hierarchy.map((item, index) => item === null ? null : {
-                val: item,
+            container.push(item.map((it, index) => it === null ? null : {
+                val: it,
                 left: container[i - 1][2 * index],
                 right: container[i - 1][2 * index + 1]
             }))
@@ -73,12 +44,12 @@ const toLinkedList = (list: number[]): ListNode | null => {
     if (len === 0) {
         return null
     }
-    const initListNode: ListNode = {val: -1, next: null}
-    const result: ListNode = {...initListNode}
+    const initListNode: ListNode = { val: -1, next: null }
+    const result: ListNode = { ...initListNode }
     let temp: ListNode | null = result
     for (let i = 0; i < list.length; i++) {
         temp.val = list[i]
-        temp.next = (i !== len - 1) ? {...initListNode} : null
+        temp.next = (i !== len - 1) ? { ...initListNode } : null
         temp = temp.next
     }
     return result
