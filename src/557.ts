b1187a511add18fe0b33391889c9ecbe15467bdf
ZSX-DB <50871520+ZSX-DB@users.noreply.github.com>
2022-12-16 23:00:39 +0800

    add: 557

---
+++ src/557.ts
@@ -0,0 +1,11 @@
+const reverseWords = (s: string): string => s
+    .split(' ')
+    .map(word => {
+        let newWord = ''
+        for (let i = word.length - 1; i >= 0; i--) {
+            newWord += word[i]
+        }
+        return newWord
+    }).join(' ')
+
+export default reverseWords
---
+++ src/tests/557.spec.ts
@@ -0,0 +1,6 @@
+import reverseWords from '../557';
+
+test("557", () => {
+    expect(reverseWords("Let's take LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc")
+    expect(reverseWords("God Ding")).toBe("doG gniD")
+})
